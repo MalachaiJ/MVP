ba6aaecc6b2f009549a7b8642b0b316185b9b227
Makachai Jacobs <malachai.jacobs15@gmail.com>
2021-10-25 10:44:34 -0600

    MVP

--- Alert/ViewController.swift
+++ Alert/ViewController.swift
@@ -7,13 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func subButton(_ sender: Any)
+    {  //Create Alert
+        let alert = UIAlertController (title: "Alert!", message: "Please Enter Text!", preferredStyle: .alert)
+        
+        //Put Ok button on alert
+        let ok =
+        UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(ok)
+        //put cancel on alert
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alert.addAction(cancel)
+        //present alert
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+      
+  
+    
 }
 
